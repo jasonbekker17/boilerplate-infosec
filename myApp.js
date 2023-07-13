2f7b4108940de76ec2c924c1091e1c8e83582c41
Jason Bekker <jasonbekker45@gmail.com>
2023-07-13 14:55:14 +0300

    made changes to helmet

--- myApp.js
+++ myApp.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 
-
-
-app.use(helmet.hidePoweredBy())
+app.use(
+  helmet({
+    xPoweredBy: false,
+  })
+);
 
 
 
